fb95d82610aaf7ab0e0ef2ecf0f6e6f075c8fda7
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-25 16:59:35 +0300

    Update build.gradle.kts

--- hw1/build.gradle.kts
+++ hw1/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+        runtimeOnly("com.h2database:h2")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.withType<KotlinCompile> {
